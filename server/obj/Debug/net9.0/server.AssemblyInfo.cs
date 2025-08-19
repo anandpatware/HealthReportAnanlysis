a1f3bb280e8fc43bca29fc136bb42847aef9cd66
anandpat <anandpatware123@gmail.com>
2025-08-19 22:16:12 +0530

    jhg

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce1cbae4fc6ebc57e64dc49c07d3a310c0a43d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e59662d6d0a4fbbc8795bca2c151f38d7326aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
